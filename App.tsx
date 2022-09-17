797064ebc7e5ac31dc2d52bfda3a694899f5b652
fanjasoa <fanjasoa18@gmail.com>
2022-09-17 10:39:45 +0300

    feat: add app file

---
+++ App.tsx
@@ -0,0 +1,28 @@
+
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Login } from './components/Login';
+import { Event } from './components/Event';
+import { EventList } from './components/EventListComponent';
+import { EventListAdd } from './components/EventList';
+import { Navbar } from './components/NavBarComponent';
+import { Presence } from './components/Presence';
+import { StudentList } from './components/StudentList';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/home' element={<Event/>}/>
+        <Route path='/event' element={<EventListAdd/>}/>
+        <Route path='/card' element={<EventList/>}/>
+        <Route path='/presence' element={<Presence/>}/>
+        <Route path='/student' element={<StudentList/>}/>
+      </Routes>
+    </>
+    
+  );
+}
+
+export default App;
